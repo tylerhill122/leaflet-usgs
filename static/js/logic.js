023dcb119f0d79afb3959422267e007a77f41eb3
Tyler Hill <tylergenehill122@gmail.com>
2022-07-09 20:56:33 -0400

    default baseMap

--- static/js/logic.js
+++ static/js/logic.js
@@ -101,7 +101,11 @@ d3.json(url).then(function(data) {
         var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
             attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
         });
-    
+
+        topo.addTo(myMap)
+        
+        myMap.addLayer(eqLayer)
+
         var baseMaps = {
             "Street": street,
             "Topography": topo
@@ -114,7 +118,4 @@ d3.json(url).then(function(data) {
 
     L.control.layers(baseMaps, overlayMaps).addTo(myMap);
     });
-
-
-
 });
