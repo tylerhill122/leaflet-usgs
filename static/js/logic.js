e37b8b34d2383caf8e2abb97d02949f3549bc54e
Tyler Hill <tylergenehill122@gmail.com>
2022-07-16 17:46:15 -0400

    cleaning

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,56 +1,50 @@
+// USGS data source
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-// var url = "/static/data/all_week.geojson";
-// var url = "/static/data/significant_month.geojson";
 
-// earthquakes > 4.5 magnitude in the past 7 days
-// var url = "/static/data/4.5_week.geojson";
-
+// import data with d3
 d3.json(url).then(function(data) {
 
-    // L.geoJson(data).addTo(myMap);
-
     var earthQuakes = [];
-
     let dF = data.features;
-    // let depth = dF[i].geometry.coordinates[2]
 
     for (var i = 0; i < dF.length; i++) {
         // Conditionals for EQ depth to set color
         var color = "";
-        if (dF[i].geometry.coordinates[2] > 400) {
+        var mag = dF[i].geometry.coordinates[2];
+        if (mag > 400) {
             color = "#2a4858";
         }
-        else if (dF[i].geometry.coordinates[2] < 400 && dF[i].geometry.coordinates[2] > 300) {
+        else if (mag < 400 && mag > 300) {
             color = "#23596a";
         }
-        else if (dF[i].geometry.coordinates[2] < 300 && dF[i].geometry.coordinates[2] > 200) {
+        else if (mag < 300 && mag > 200) {
             color = "#146b79";
         }
-        else if (dF[i].geometry.coordinates[2] < 300 && dF[i].geometry.coordinates[2] > 200) {
+        else if (mag < 300 && mag > 200) {
             color = "#007d85";
         }
-        else if (dF[i].geometry.coordinates[2] < 200 && dF[i].geometry.coordinates[2] > 100) {
+        else if (mag < 200 && mag > 100) {
             color = "#008f8c";
         }
-        else if (dF[i].geometry.coordinates[2] < 100 && dF[i].geometry.coordinates[2] > 50) {
+        else if (mag < 100 && mag > 50) {
             color = "#0ea18f";
         }
-        else if (dF[i].geometry.coordinates[2] < 50 && dF[i].geometry.coordinates[2] > 30) {
+        else if (mag < 50 && mag > 30) {
             color = "#35b28e";
         }
-        else if (dF[i].geometry.coordinates[2] < 30 && dF[i].geometry.coordinates[2] > 20) {
+        else if (mag < 30 && mag > 20) {
             color = "#58c389";
         }
-        else if (dF[i].geometry.coordinates[2] < 20 && dF[i].geometry.coordinates[2] > 10) {
+        else if (mag < 20 && mag > 10) {
             color = "#7dd382";
         }
-        else if (dF[i].geometry.coordinates[2] < 10 && dF[i].geometry.coordinates[2] > 5) {
+        else if (mag < 10 && mag > 5) {
             color = "#a4e27a";
         }
-        else if (dF[i].geometry.coordinates[2] < 5 && dF[i].geometry.coordinates[2] > 2) {
+        else if (mag < 5 && mag > 2) {
             color = "#cdef72";
         }
-        else if (dF[i].geometry.coordinates[2] < 2 && dF[i].geometry.coordinates[2] > 0) {
+        else if (mag < 2 && mag > 0) {
             color = "#fafa6e";
         }
         else {
@@ -75,7 +69,7 @@ d3.json(url).then(function(data) {
                     .bindPopup(`
                         <h2>${dF[i].properties.place}</h2><hr>
                         <h3>Magnitude:</h3> ${dF[i].properties.mag}
-                        <h3>Depth:</h3> ${dF[i].geometry.coordinates[2]}
+                        <h3>Depth:</h3> ${mag}
                         <div class="logo">
                         <a href="${dF[i].properties.url}" target="_blank"><img src="static/images/usgs-logo.png" id="usgs"><p>Learn more</p></a>
                         </div>
